a395cb812e03c6bd1555cd1ecea0982a06f6b4c5
Yann <yann.sanquer@epitech.fr>
2022-01-19 13:53:05 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Index from './src/components/Index';
+
+export default function App() {
+  return <Index />;
+}
---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Index() {
+  return (
+    <>
+      <View>
+        <Text>Hello Baroudeur !</Text>
+        
+      </View>
+    </>
+  );
+}
